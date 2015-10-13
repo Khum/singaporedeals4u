0d6a7779e00fc87e283f3bc062a3a473f7d995e4
Khum <kkhan@tkbench.com>
2015-10-13 13:33:10 +0500

    10/13/2015

--- head.php
+++ head.php
@@ -5,7 +5,7 @@
     
 	
     <!-- Favicons-->
-    <link rel="shortcut icon" href="<?=site_url('img/favicon.ico');?>" type="image/x-icon">
+    <link rel="shortcut icon" href="<?=site_url('img/fav.ico');?>" type="image/x-icon">
     <link rel="apple-touch-icon" type="image/x-icon" href="<?=site_url('img/apple-touch-icon-57x57-precomposed.png');?>">
     <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="<?=site_url('img/apple-touch-icon-72x72-precomposed.png');?>">
     <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="<?=site_url('img/apple-touch-icon-114x114-precomposed.png');?>">
